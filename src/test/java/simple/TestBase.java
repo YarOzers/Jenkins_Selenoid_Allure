d2f68fb35c9bf14f16f47398f12f6a14792bdadd
Yarik Ozerskiy <ozerskiy.yaroslav@gmail.com>
2023-11-02 19:31:01 +0400

    added allure listeners and steps.

--- src/test/java/simple/TestBase.java
+++ src/test/java/simple/TestBase.java
@@ -1,9 +1,10 @@
 package simple;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -11,19 +12,14 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import java.util.Map;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-@Tag("remote")
-public class Positive {
+public class TestBase {
     @BeforeAll
     public static void setupBrowser() {
         Configuration.browser = "chrome";
         System.setProperty("webdriver.chrome.driver", "C:\\ChromeDriver\\chromedriver-win64\\chromedriver.exe");
-    }
-    @Test
-    void testTranslate1(){
         Configuration.browserSize = "1920x1080";
         Configuration.remote ="http://95.31.234.122:8080/wd/hub";
         Configuration.browserVersion = "118";
@@ -31,15 +27,14 @@ public class Positive {
 //        capabilities.setCapability("browserName", "chrome");
 //        capabilities.setCapability("browserVersion", "118.0");
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                        "enableVNC", true,
-                        "enebleVideo", false
-                ));
+                "enableVNC", true,
+                "enebleVideo", false
+        ));
         Configuration.browserCapabilities = capabilities;
-
-        open("https://wooordhunt.ru/word/word");
-        $("#hunted_word").setValue("waterfall");
-        $("#hunted_word_submit").click();
-        $("#wd_title").$("h2").shouldHave(text("Waterfall"));
+    }
+    @BeforeEach
+    void addListener(){
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
 }
---
+++ src/test/java/simple/TranslateTest.java
@@ -0,0 +1,30 @@
+package simple;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
+public class TranslateTest extends TestBase{
+
+    @Test
+    @Tag("remote")
+    void testOne(){
+        step("Open website", ()->{
+            open("https://wooordhunt.ru/word/word");
+        });
+        step("Enner word:'waterfall'",()->{
+            $("#hunted_word").setValue("waterfall");
+        });
+        step("Click Find",()->{
+            $("#hunted_word_submit").click();
+        });
+        step("Lable contains text 'Waterfall'",()->{
+            $("#wd_title").$("h2").shouldHave(text("Waterfall"));
+        });
+
+    }
+}
