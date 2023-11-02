2a04e648a4708aa787cfcc3d2a487be53869a116
Yarik Ozerskiy <ozerskiy.yaroslav@gmail.com>
2023-11-02 18:12:47 +0400

    added remote test

--- src/test/java/simple/Positive.java
+++ src/test/java/simple/Positive.java
@@ -1,48 +1,45 @@
 package simple;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.util.Map;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-@Tag("simple")
+@Tag("remote")
 public class Positive {
+    @BeforeAll
+    public static void setupBrowser() {
+        Configuration.browser = "chrome";
+        System.setProperty("webdriver.chrome.driver", "C:\\ChromeDriver\\chromedriver-win64\\chromedriver.exe");
+    }
     @Test
-    void test1(){
-        assertTrue(true);
+    void testTranslate1(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote ="http://95.31.234.122:8080/wd/hub";
+        Configuration.browserVersion = "118";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("browserName", "chrome");
+//        capabilities.setCapability("browserVersion", "118.0");
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                        "enableVNC", true,
+                        "enebleVideo", false
+                ));
+        Configuration.browserCapabilities = capabilities;
+
+        open("https://wooordhunt.ru/word/word");
+        $("#hunted_word").setValue("waterfall");
+        $("#hunted_word_submit").click();
+        $("#wd_title").$("h2").shouldHave(text("Waterfall"));
     }
 
-    @Test
-    void test2(){
-        assertTrue(true);
-    }
-
-    @Test
-    void test3(){
-        assertTrue(true);
-    }
-
-    @Test
-    void test4(){
-        assertTrue(true);
-    }
-
-    @Test
-    void test5(){
-        assertTrue(true);
-    }
-
-    @Test
-    void test6(){
-        assertTrue(true);
-    }
-
-    @Test
-    void test7(){
-        assertTrue(true);
-    }
-
-    @Test
-    void test8(){
-        assertTrue(true);
-    }
 }
--- src/test/java/simple/SkippedTest.java
+++ src/test/java/simple/SkippedTest.java
@@ -1,5 +1,6 @@
 package simple;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -7,21 +8,25 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @Tag("simple")
 public class SkippedTest {
     @Test
+    @Disabled
     void skippedTest1(){
         assertTrue(true);
     }
 
     @Test
+    @Disabled
     void skippedTest2(){
         assertTrue(true);
     }
 
     @Test
+    @Disabled("Some reason")
     void skippedTest3(){
         assertTrue(true);
     }
 
-    @Test
+    @Test@Disabled("Some reason")
+
     void skippedTest4(){
         assertTrue(true);
     }
